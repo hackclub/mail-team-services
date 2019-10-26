d653f6abc84dc1e5b80912bec11b45176f722616
Christopher Walker <cwalker@polytrope.com>
2019-10-26 17:33:40 -0400

    FIRST POST!!!!!!!!!!

---
+++ index.js
@@ -0,0 +1,36 @@
+const FormData = require('form-data')
+const fetch = require('node-fetch')
+const express = require('express')
+const data = require('./data.js')
+
+const app = express()
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+app.post('/bounce', function (req, res) {
+    const {
+        fileData,
+        fileType,
+        fileName,
+        message,
+        token,
+        channel
+    } = req.params
+
+    var buffer = new Buffer(fileData, 'base64')
+    var form = new FormData()
+
+    form.append('file', buffer)
+    form.append('token', token)
+    form.append('channels', channel)
+    form.append('filename', fileName)
+    form.append('filetype', fileType)
+    form.append('initial_comment', message)
+    
+    form.submit('https://slack.com/api/files.upload', function(err, response) {
+        if (err) res.send(err)
+        else res.send('success')
+    })
+})
