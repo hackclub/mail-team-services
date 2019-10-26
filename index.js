f2467400afaff20322ad7cca2c085fafdacdde16
Christopher Walker <cwalker@polytrope.com>
2019-10-26 17:49:51 -0400

    Add crappy error handling

--- index.js
+++ index.js
@@ -8,27 +8,32 @@ app.listen(3000, () => {
 });
 
 app.post('/bounce', function (req, res) {
-    const {
-        fileData,
-        fileType,
-        fileName,
-        message,
-        token,
-        channel
-    } = req.params
+    try {
+        const {
+            fileData,
+            fileType,
+            fileName,
+            message,
+            token,
+            channel
+        } = req.params
 
-    var buffer = new Buffer(fileData, 'base64')
-    var form = new FormData()
+        var buffer = new Buffer(fileData, 'base64')
+        var form = new FormData()
 
-    form.append('file', buffer)
-    form.append('token', token)
-    form.append('channels', channel)
-    form.append('filename', fileName)
-    form.append('filetype', fileType)
-    form.append('initial_comment', message)
-    
-    form.submit('https://slack.com/api/files.upload', function(err, response) {
-        if (err) res.send(err)
-        else res.send('success')
-    })
+        form.append('file', buffer)
+        form.append('token', token)
+        form.append('channels', channel)
+        form.append('filename', fileName)
+        form.append('filetype', fileType)
+        form.append('initial_comment', message)
+        
+        form.submit('https://slack.com/api/files.upload', function(err, response) {
+            if (err) res.send(err)
+            else res.send('success')
+        })
+    }
+    catch (err) {
+        res.send(err)
+    }
 })
