038bba106e7c0c892682cd72c927693ad5b0da76
Christopher Walker <cwalker@polytrope.com>
2019-10-26 17:59:32 -0400

    Add logging

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 app.post('/bounce', function (req, res) {
+    console.log('oh boy oh boy here comes a request!!')
+
     try {
         const {
             fileData,
@@ -18,8 +20,12 @@ app.post('/bounce', function (req, res) {
             channel
         } = req.params
 
+        console.log('hmmm, lots of stuff to unpack here...')
+
         var buffer = Buffer.from(fileData, 'base64')
         var form = new FormData()
+
+        console.log('ok i made a buffer and a form')
 
         form.append('file', buffer)
         form.append('token', token)
@@ -27,13 +33,23 @@ app.post('/bounce', function (req, res) {
         form.append('filename', fileName)
         form.append('filetype', fileType)
         form.append('initial_comment', message)
+
+        console.log('yay everything is appended to the form! redy to send :))')
         
         form.submit('https://slack.com/api/files.upload', function(err, response) {
-            if (err) res.send(err)
-            else res.send('success')
+            if (err) {
+                console.log('i submitted but i got error :(')
+                res.send(err)
+            }
+            else {
+                console.log('i submitted and it is good so happy!!!!!')
+                res.send('success')
+            }
         })
     }
     catch (err) {
+        console.log('ummmmm something bad hapend :(((')
+        console.log(err)
         res.send(err)
     }
 })
