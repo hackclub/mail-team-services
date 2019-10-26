ddf5d762578a9a42d20837e87c4ef36000859dbd
Christopher Walker <cwalker@polytrope.com>
2019-10-26 17:52:39 -0400

    Listen on heroku-assigned port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
 
