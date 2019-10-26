2e79c07f5ecdfb89a188433c1bd60fde805b359f
Christopher Walker <cwalker@polytrope.com>
2019-10-26 18:07:30 -0400

    Use application/json

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
