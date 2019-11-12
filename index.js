20a850fdb2184101bd6ae0e04cb4d81837fee976
Christopher Walker <cwalker@polytrope.com>
2019-11-12 16:47:09 -0500

    Fix crash on empty notes

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.post('/shipping-label', async function (req, res) {
         var { width, height } = firstPage.getSize()
         
         var addendumText = scenarioName +
-            (missionNote.length ? ' | '+missionNote : '')
+            (missionNote ? ' | '+missionNote : '')
 
         firstPage.drawText(addendumText, {
             x: 10,
