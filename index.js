1ffa92d0f824d7b035fbf046e13b08e1ef4a3e6c
Christopher Walker <cwalker@polytrope.com>
2019-11-27 13:58:59 -0500

    added some shit from stack overflow to fix my problem with request bodies

--- index.js
+++ index.js
@@ -24,7 +24,15 @@ const fetchMailMission = async id => await mailMissionsTable.read({
 })[0];
 
 const app = express()
+
 app.use(express.json())
+app.use (express.urlencoded({extended: false}))
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
